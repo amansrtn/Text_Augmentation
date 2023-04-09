a3b361cd853eb16a9cafe4da24938c5dc4097747
Aman kumar singh <103808097+amansrtn@users.noreply.github.com>
2023-04-10 01:59:32 +0530

    Update Text_Augmentation.py

--- Text_Augmentation.py
+++ Text_Augmentation.py
@@ -2,7 +2,7 @@ import csv
 from textaugment import Wordnet
 
 # read the CSV file
-with open('Msg1.csv', 'r') as file:
+with open('data1.csv', 'r') as file:
     reader = csv.DictReader(file)
     rows = [row for row in reader]
 
@@ -22,7 +22,7 @@ for row in rows:
         new_rows.append(new_row)
 
 # append the new rows to the original CSV file
-with open('Msg1.csv', 'a') as file:
+with open('data1.csv', 'a') as file:
     writer = csv.DictWriter(file, fieldnames=reader.fieldnames)
     for new_row in new_rows:
         writer.writerow(new_row)
